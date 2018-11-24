696dcdce960320bd61d166631bf7f57c6b6d91f6
shu-kobayashi <hashi.call2@gmail.com>
2018-11-24 15:21:08 +0900

    test.phpを追加

--- test.php
+++ test.php
@@ -1,2 +1,21 @@
 <?php
-phpinfo();
+/**
+ * Created by PhpStorm.
+ * User: s16C1
+ * Date: 2018/11/24
+ * Time: 14:55
+ */
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Cheack username</title>
+</head>
+<body>
+<form action="" action="POST">
+    <input type="text" name="username" placeholder="user name">
+    <input type="submit" value="Check!">
+</form>
+</body>
+</html>

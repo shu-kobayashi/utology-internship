831b2956fb5666c5b3def4f396e3e89617d96402
shu-kobayashi <hashi.call2@gmail.com>
2018-11-24 15:35:52 +0900

    test.phpを追加

--- test.php
+++ test.php
@@ -5,6 +5,14 @@
  * Date: 2018/11/24
  * Time: 14:55
  */
+$username = '';
+if($_SERVER['REQUEST_METHOD']==='POST'){
+    $username = $_POST['username'];
+    $err = false;
+    if(strlen($username)>8){
+        $err = true;
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -14,7 +22,8 @@
 </head>
 <body>
 <form action="" action="POST">
-    <input type="text" name="username" placeholder="user name">
+    <input type="text" name="username" placeholder="user name"value="<?php echo
+    htmlspecialchars($username,ENT_QUOTES,'UTF-8'); ?>">
     <input type="submit" value="Check!">
 </form>
 </body>

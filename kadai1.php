6a9c55fa2ea3bc75d1f6d10d2e636bc4a6ec3520
shu-kobayashi <hashi.call2@gmail.com>
2018-12-15 16:06:36 +0900

    kadai1の変更1

--- kadai1.php
+++ kadai1.php
@@ -2,14 +2,11 @@
 <title>Workflow Registration</title>
 <body>
 <p>You entered:</p>
-
 <?php
 $username = "nick";
 $password = "mypassword";
-
 echo "<p>Username = " . $username . "</p>";
 echo "<p>Password = " . $password . "</p>";
 ?>
-
 </body>
 </html>

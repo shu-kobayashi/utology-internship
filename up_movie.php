ea7e81b913b6c89edd6aefe13d4f570b26d12818
shu-kobayashi <hashi.call2@gmail.com>
2019-02-02 14:57:23 +0900

    test

--- up_movie.php
+++ up_movie.php
@@ -6,4 +6,6 @@ if($_FILES['file']){
 <form action="./normal_post1.php" method="POST" enctype="multipart/form-data">
     <input type="file" name="file">
     <input type="submit" value="ファイルをアップロードする">
+
+
 </form>

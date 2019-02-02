a9b2015d2b67eb5b017defb19fad098a1bd2718f
shu-kobayashi <hashi.call2@gmail.com>
2019-02-02 14:30:35 +0900

    ファイルのアップロード

--- up_movie.php
+++ up_movie.php
@@ -3,6 +3,7 @@ if($_FILES['file']){
     move_uploaded_file($_FILES['file']['tmp_name'], './img/test.jpg');
 }
 ?>
+
 <form action="./normal_post1.php" method="POST" enctype="multipart/form-data">
     <input type="file" name="file">
     <input type="submit" value="ファイルをアップロードする">

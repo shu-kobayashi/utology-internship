7fd31ac57d1c495aa6fe9918c35066eac2b2a91b
shu-kobayashi <hashi.call2@gmail.com>
2019-07-04 01:57:08 +0900

    mysql

--- database.php
+++ database.php
@@ -3,4 +3,5 @@ $DBNAME = "test";
 $DBUSER = "shu";
 $DBPASSWD = "";
 $dsn = "mysql:host={$DBSERVER};dbname={$DBSERVER};charset=utf8";
+
 $pdo = new \PDO($dsn, $DBUSER, $DBPASSWD, array(\PDO::ATTR_EMULATE_PREPARES => false));

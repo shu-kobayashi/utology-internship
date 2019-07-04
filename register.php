5a84eb6635670d7fb1486c0ab5782da8319772d2
shu-kobayashi <hashi.call2@gmail.com>
2019-07-04 23:05:08 +0900

    mysqll

--- register.php
+++ register.php
@@ -6,12 +6,14 @@ try {
     $DBSERVER = 'localhost';
     $DBUSER = 'shu';
     $DBPASSWD = '';
-    $DBNAME = 'sampledb';
+    /*$DBNAME = 'sampledb';
     $dsn = 'mysql:'
         . 'host=' . $DBSERVER . ';'
         . 'dbname=' . $DBNAME . ';'
-        . 'charset=utf8';
-    $pdo = new PDO($dsn, $DBUSER, $DBPASSWD, array(PDO::ATTR_EMULATE_PREPARES => false));
+        . 'charset=utf8';*/
+    $dsn = 'mysql:dbname=sampledb;host=localhost';
+    //$pdo = new PDO($dsn, $DBUSER, $DBPASSWD, array(PDO::ATTR_EMULATE_PREPARES => false));
+    $pdo = new PDO($dsn, $DBUSER, $DBPASSWD);
 } catch (Exception $e) {
     $message = "接続に失敗: {$e->getMessage()}";
 }
@@ -20,10 +22,10 @@ if(!empty($_POST['name']) && !empty($_POST['mail']) && !empty($_POST['password']
     $name = $_POST['name'];
     $mail = $_POST['mail'];
     $password = $_POST['password'];
-
-
-    $sql = 'INSERT INTO board (name, mail, password, created, modified)';
-    $sql .= ' VALUES (:name, :mail, :password, NOW(), NOW())';
+    //$sql = "INSERT INTO board (name, mail, password, created, modified)";
+    //$sql .= ' VALUES (:name, :mail, :password, NOW(), NOW())';
+    $sql = "INSERT INTO makeuser (name, mail, password)";
+    $sql .= ' VALUES (:name, :mail, :password)';
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(':name', $name, \PDO::PARAM_STR);
     $stmt->bindValue(':mail', $mail, \PDO::PARAM_STR);
@@ -31,7 +33,7 @@ if(!empty($_POST['name']) && !empty($_POST['mail']) && !empty($_POST['password']
     $result = $stmt->execute();
     if($result) {
         $message = 'ユーザーを作成';
-        $_SESSION['id'] = $pdo->lastInsertId();
+        //$_SESSION['id'] = $pdo->lastInsertId();
         $_SESSION['name'] = $name;
         $_SESSION['mail'] = $mail;
         $_SESSION['password'] = $password;
@@ -44,26 +46,29 @@ if(!empty($_POST['name']) && !empty($_POST['mail']) && !empty($_POST['password']
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>新規作成</title>
+    <title>ユーザー登録</title>
 </head>
 <body>
 <header>
+    <!--
     <div>
+
         <a href="/board/index.php">TOP</a>
         <a href="/board/register.php">新規作成</a>
         <a href="/board/login.php">ログイン</a>
         <a href="/board/logout.php">ログアウト</a>
     </div>
-    <h1>新規作成</h1>
+    -->
+    <h1>ユーザーの新規登録</h1>
 </header>
 <div>
     <div style="color: purple">
         <?php echo $message; ?>
     </div>
     <form action="register.php" method="post">
-        <label>メールアドレス: <input type="email" name="mail"/></label><br/>
-        <label>パスワード: <input type="password" name="password"/></label><br/>
-        <label>名前: <input type="text" name="name"/></label><br/>
+        <label>mail: <input type="email" name="mail"/></label><br/>
+        <label>password: <input type="password" name="password"/></label><br/>
+        <label>name: <input type="text" name="name"/></label><br/>
         <input type="submit" value="新規登録">
     </form>
 </div>

f3ec1f131518316b6a5dfedf34165318a1da33eb
shu-kobayashi <hashi.call2@gmail.com>
2019-07-10 18:24:55 +0900

    mysql

--- register.php
+++ register.php
@@ -4,16 +4,16 @@ session_start();
 $message = '';
 try {
     $DBSERVER = 'localhost';
-    $DBUSER = 'shu';
-    $DBPASSWD = '';
-    /*$DBNAME = 'sampledb';
+    $DBUSER = 'board';
+    $DBPASSWD = 'boardpw';
+    $DBNAME = 'sampledb';
     $dsn = 'mysql:'
         . 'host=' . $DBSERVER . ';'
         . 'dbname=' . $DBNAME . ';'
         . 'charset=utf8';*/
-    $dsn = 'mysql:dbname=sampledb;host=localhost;charset=utf8';
-    //$pdo = new PDO($dsn, $DBUSER, $DBPASSWD, array(PDO::ATTR_EMULATE_PREPARES => false));
-    $pdo = new PDO($dsn, $DBUSER, $DBPASSWD);
+    //$dsn = 'mysql:dbname=sampledb;host=localhost;charset=utf8';
+    $pdo = new PDO($dsn, $DBUSER, $DBPASSWD, array(PDO::ATTR_EMULATE_PREPARES => false));
+    //$pdo = new PDO($dsn, $DBUSER, $DBPASSWD);
 } catch (Exception $e) {
     $message = "接続に失敗: {$e->getMessage()}";
 }
@@ -22,22 +22,22 @@ if(!empty($_POST['name']) && !empty($_POST['mail']) && !empty($_POST['password']
     $name = $_POST['name'];
     $mail = $_POST['mail'];
     $password = $_POST['password'];
-    //$sql = "INSERT INTO board (name, mail, password, created, modified)";
-    //$sql .= ' VALUES (:name, :mail, :password, NOW(), NOW())';
-    $sql = "INSERT INTO makeuser (name, email, password)";
-    $sql .= ' VALUES (:name, :mail, :password)';
+    $sql = "INSERT INTO board (name, mail, password, created, modified)";
+    $sql .= ' VALUES (:name, :mail, :password, NOW(), NOW())';
+    //$sql = "INSERT INTO makeuser (name, email, password)";
+    //$sql .= ' VALUES (:name, :mail, :password)';
     $stmt = $pdo->prepare($sql);
-    //$stmt->bindValue(':name', $name, \PDO::PARAM_STR);
-    //$stmt->bindValue(':mail', $mail, \PDO::PARAM_STR);
-    //$stmt->bindValue(':password', $password, \PDO::PARAM_STR);
-    $stmt->bindValue(':name', $name);
-    $stmt->bindValue(':mail', $mail);
-    $stmt->bindValue(':password', $password);
+    $stmt->bindValue(':name', $name, \PDO::PARAM_STR);
+    $stmt->bindValue(':mail', $mail, \PDO::PARAM_STR);
+    $stmt->bindValue(':password', $password, \PDO::PARAM_STR);
+    //$stmt->bindValue(':name', $name);
+    //$stmt->bindValue(':mail', $mail);
+    //$stmt->bindValue(':password', $password);
     //下の行で行を追加すると思われる
     $result = $stmt->execute();
     if ($result) {
         $message = 'ユーザーを作成';
-        //$_SESSION['id'] = $pdo->lastInsertId();
+        $_SESSION['id'] = $pdo->lastInsertId();
         $_SESSION['name'] = $name;
         $_SESSION['mail'] = $mail;
         $_SESSION['password'] = $password;
@@ -58,15 +58,13 @@ if(!empty($_POST['name']) && !empty($_POST['mail']) && !empty($_POST['password']
 </head>
 <body>
 <header>
-    <!--
-    <div>
 
-        <a href="/board/index.php">TOP</a>
-        <a href="/board/register.php">新規作成</a>
-        <a href="/board/login.php">ログイン</a>
-        <a href="/board/logout.php">ログアウト</a>
+    <div>
+        <a href="./index.php">TOP</a>
+        <a href="./register.php">新規作成</a>
+        <a href="./login.php">ログイン</a>
+        <a href="./logout.php">ログアウト</a>
     </div>
-    -->
     <h1>ユーザーの新規登録</h1>
 </header>
 <div>
